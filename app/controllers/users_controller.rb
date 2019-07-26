ea1d60edb41486356dd6eee6d35ddc6d4231c79a
設定したいユーザー名をここに入力してください <>
2019-07-26 15:56:31 +0000

    Finish full-fledged-login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,40 @@
-def create
-  @user = User.new(user_params)
-  if @user.save
-    log_in @user # 保存成功後、ログインします。
-    flash[:success] = '新規作成に成功しました。'
-    redirect_to @user
-  else
-    render :new
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合の処理を記述します。
+    else
+      render :edit      
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
