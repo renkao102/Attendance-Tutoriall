8b2fda8830ad018b7bbf08471e67336c4d7840a3
設定したいユーザー名をここに入力してください <>
2019-07-26 15:40:24 +0000

    Finish full-fledged-login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,25 +1,6 @@
-module SessionsHelper
-
-  # 引数に渡されたユーザーオブジェクトでログインします。
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  # セッションと@current_userを破棄します
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
-  # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
-  def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
-
-  # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
-  def logged_in?
-    !current_user.nil?
-  end
+def destroy
+  # ログイン中の場合のみログアウト処理を実行します。
+  log_out if logged_in?
+  flash[:success] = 'ログアウトしました。'
+  redirect_to root_url
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
-class User < ApplicationRecord
-  before_save { self.email = email.downcase }
-
-  validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 100 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: true    
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+# トークンがダイジェストと一致すればtrueを返します。
+def authenticated?(remember_token)
+  # ダイジェストが存在しない場合はfalseを返して終了します。
+  return false if remember_digest.nil?
+  BCrypt::Password.new(remember_digest).is_password?(remember_token)
 end
