82e891d3f7e1b47bb7126949051da18160381c61
設定したいユーザー名をここに入力してください <>
2019-07-27 08:46:58 +0000

    Finish editing-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # トークンがダイジェストと一致すればtrueを返します。
-def authenticated?(remember_token)
+def 
+  authenticated?(remember_token)
   # ダイジェストが存在しない場合はfalseを返して終了します。
   return false if remember_digest.nil?
-  BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 end
+
